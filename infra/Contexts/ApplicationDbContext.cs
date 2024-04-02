a019d3b103ef3877e7c661b466a939b49c5211a3
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2024-04-02 11:27:10 -0300

    Feat: Adiciona remodelagem da arquitetura

---
+++ infra/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TransacoesProjeto.Models;
+
+namespace TransacoesProjeto.Data.context
+{
+    public class ApplicationDbContext(IConfiguration configuration) : DbContext
+    {
+
+        protected readonly IConfiguration Configuration = configuration;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+
+    }
+}
